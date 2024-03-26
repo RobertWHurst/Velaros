53989747f9cf6de882fb9fb250eb8a4e09129039
Robert Hurst <robertwhurst@gmail.com>
2024-03-25 17:11:36 -0700

    stub out router struct

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/RobertWHurst/scramjet
 
 go 1.21.4
+
+require github.com/RobertWHurst/navaros v1.0.0-beta-9
+
+require github.com/grafana/regexp v0.0.0-20221123153739-15dc172cd2db // indirect
--- router.go
+++ router.go
@@ -1,1 +1,21 @@
 package scramjet
+
+import (
+	"net/http"
+
+	"github.com/RobertWHurst/navaros"
+)
+
+type Router struct{}
+
+func NewRouter() *Router {
+	return &Router{}
+}
+
+func (r *Router) Handle(ctx *navaros.Context) {
+
+}
+
+func (r *Router) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+}
